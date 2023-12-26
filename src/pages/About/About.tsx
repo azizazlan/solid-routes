c689d2e0ea06d8c05dbd0dab726f46f8ee50dbea
Azlan <azlan@thuleen.io>
2023-12-26 17:33:31 +0800

    Added margin in nav css.

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -2,7 +2,10 @@ export default function About() {
   return (
     <div>
       <h1>About</h1>
-      <p>This is my boilerplate to configure eslint and prettier in vscode.</p>
+      <p>
+        This is my <code>solidjs</code> boilerplate to configure eslint and
+        prettier in vscode.
+      </p>
     </div>
   );
 }
